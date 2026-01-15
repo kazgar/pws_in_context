f688c866bb09361a2df14e7169f76d334e416b2b
Kazik <kazik@example.com>
2026-01-15 12:53:24 +0100

    added a bunch of different models and made prefix extraction robust for all

--- pws_in_context/load_model.py
+++ pws_in_context/load_model.py
@@ -1,14 +1,38 @@
 import torch
 import torch.nn as nn
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+)
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 
 models = {
-    "Llama": "meta-llama/Llama-3.2-1B",
-    "GPT2": "openai-community/gpt2",
+    "Llama-3.2-1B": "meta-llama/Llama-3.2-1B",  # 1B parameters | 8 languages
+    "gpt2": "openai-community/gpt2",  # 0.1B parameters | English
+    "bloom-1b1": "bigscience/bloom-1b1",  # 1B parameters | 48 languages
+    "bloom-1b7": "bigscience/bloom-1b7",  # 2B parameters | 48 languages
+    "opt-125m": "facebook/opt-125m",  # 125M parameters | English
+    "opt-1.3b": "facebook/opt-1.3b",  # 1.3B parameters | English
+    "falcon-7b": "tiiuae/falcon-7b",  # 7B parameters | English
+    "falcon-40b": "tiiuae/falcon-40b",  # 42B parameters | 4 languages
+    "pythia-70m": "EleutherAI/pythia-70m",  # 96M parameters | English
+    "pythia-1.4b": "EleutherAI/pythia-1.4b",  # 2B parameters | English
+    "pythia-6.9b": "EleutherAI/pythia-6.9b",  # 7B parameters | English
 }
 
-_whitespace = {"Llama": "Ġ", "GPT2": None}  # TODO
+_whitespace = {
+    "Llama-3.2-1B": "Ġ",
+    "gpt2": "Ġ",
+    "bloom-1b1": "Ġ",
+    "bloom-1b7": "Ġ",
+    "opt-125m": "Ġ",
+    "opt-1.3b": "Ġ",
+    "falcon-7b": None,  # TODO
+    "falcon-40b": None,  # TODO
+    "pythia-70m": "Ġ",
+    "pythia-1.4b": "Ġ",
+    "pythia-6.9b": "Ġ",
+}
 
 
 def load_llm(model_key: str = "Llama") -> tuple[nn.Module, PreTrainedTokenizerBase, str]:
